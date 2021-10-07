8ba976c25a827cacbd70ba3bc001974586ee76a7
Haro-stark <haroonrb64@hotmail.com>
2021-10-07 10:32:03 +0500

    Initial commit

--- src/main/java/com/example/Git/Project/GitProjectApplication.java
+++ src/main/java/com/example/Git/Project/GitProjectApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GitProjectApplication {
 
+
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(GitProjectApplication.class, args);
 	}
